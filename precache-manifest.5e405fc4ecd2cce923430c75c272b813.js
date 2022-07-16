ed49bb48aaef9a97a038e73fa6045f9e2802e8eb
Sabin43 <108047161+Sabin43@users.noreply.github.com>
2022-07-16 22:26:29 +0545

    Updates

---
+++ precache-manifest.5e405fc4ecd2cce923430c75c272b813.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d91ecf6b361574644f85",
+    "url": "/textutils/static/css/main.9305bcce.chunk.css"
+  },
+  {
+    "revision": "d91ecf6b361574644f85",
+    "url": "/textutils/static/js/main.d91ecf6b.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/textutils/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "644d9258531a091f02e7",
+    "url": "/textutils/static/js/2.644d9258.chunk.js"
+  },
+  {
+    "revision": "dbd7b2b3be13618030c7",
+    "url": "/textutils/static/js/runtime~main.dbd7b2b3.js"
+  },
+  {
+    "revision": "9ff6d3868eb4588d1a560473c11b7129",
+    "url": "/textutils/index.html"
+  }
+];
